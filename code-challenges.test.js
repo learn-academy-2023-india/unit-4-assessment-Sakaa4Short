decebd7e86516879833d46202ba89b556952cd0a
Sakaa4Short <klanyenek@gmail.com>
2024-01-22 09:10:06 -0800

    Completed Unit 4 Assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // UNIT 4: JavaScript Coding Practical Questions with Jest
 
+const { number } = require("yargs")
+
 // Please read all questions thoroughly. If you get stuck, please leave comments to help us understand your thought process.
 
 // Use test driven development to complete the following questions
@@ -19,8 +21,32 @@ const fibonacciLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 // b) Create the function that makes the test pass.
+describe("fibonacciLength", () => {
+    it("takes in a number and returns an array containing the Fibonacci sequence", () => {
+      expect(fibonacciLegnth(6)).toEqual([1, 1, 2, 3, 5, 8])
+      expect(fibonacciLegnth(10)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+    })
+  })
 
+  function fibonacciLegnth(num) {
+    let num1 = 1;
+    let num2 = 1;
+    let fibArr = [num1, num2];
+    for (let i = 2; i < num; i++) {
+      sum = num1 + num2;
+      num1 = num2;
+      num2 = sum;
+      fibArr.push(sum);
+    }
+    return fibArr;
+  }
+
+console.log(fibonacciLegnth(10)) 
 // Pseudo code:
+// Create a funtion that takes in a number and returns the fibonacci sequence to the length of the input
+// Take in a number and add it to the previous input
+// Take the sum and add it to the next input.
+// Console log the function with .legnth to create the expected output 
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
@@ -32,5 +58,18 @@ const votes2 = { upVotes: 2, downVotes: 33 }
 // Expected output: -31
 
 // b) Create the function that makes the test pass.
+describe("tally", () => {
+    it("takes in an object that contains up votes and down votes and returns the end tally", () => {
+      expect(tally(votes1)).toEqual(11)
+      expect(tally(votes2)).toEqual(-31)
+    })
+  })
 
+  function tally(object) {
+        return object.upVotes - object.downVotes;
+    }
+
+  console.log(tally(votes2))
 // Pseudo code:
+// Create a function that subtracts upvotes from downvotes
+// Output created is the tally of the two
--- ruby.rb
+++ ruby.rb
@@ -7,4 +7,31 @@
 # Add comments to your code explaining your approach
 
 class Bike
+    # attribute accessor is used in place of get and set values. Making pedal_faster and brake along with bike info caused issues when using rspec ruby_spec.rb so I kept it reduced to model wheels &current speed.
+    attr_accessor :model, :wheels, :current_speed
+    # Using the initialize method on everything that was placed in line 11 to initialize the super class
+    def initialize(model)
+        @model = model
+        @wheels = 2
+        @current_speed = 0
+        
+    end
+    # Setting up pedal_faster method increases the current speed 
+    def pedal_faster(speed)
+        @current_speed += speed
+    end
+    # Setting the brake method up around decreasing the current speed and preventing negative speeds 
+    def brake(speed)
+        @current_speed -= speed
+        @current_speed = 0 if current_speed < 0
+        @current_speed
+    end
+    # Setting bike_info method returns a string with the same objects utilized in the attr_accessor
+    def bike_info
+        "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph."
+    end
+
 end
+
+    # Making pedal_faster and brake along with bike info caused issues when using rspec ruby_spec.rb so I kept it reduced to model, wheels, & current speed.
+    # 
